f8a423aa09fcd43d63d2f3896878ef7ed9ebbc4d
chejian <klchejian51@qq.com>
2019-02-03 10:06:44 +0800

    update login signup

---
+++ src/resource/js/login.js
@@ -0,0 +1,15 @@
+function check() {
+    var regusername = { regstr: /^[\w]{1,6}$/, tip: '用户名请输入1-6位的英文、数字、下划线' };
+    var username = document.getElementById("username").value;
+    if (!regusername.regstr.test(username)) {
+        alert(regusername.tip)
+        // return;
+    }
+
+    var regpassword = { regstr: /^[\w]{1,6}$/, tip: '密码请输入1-6位的英文、数字、下划线' };
+    var password = document.getElementById("password").value;
+    if (!regpassword.regstr.test(password)) {
+        alert(regpassword.tip)
+        // return;
+    }
+}
